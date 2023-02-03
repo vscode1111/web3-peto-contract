4d893dee97fbce2211d026e7217f785ad14ca350
scub111nes <scub111nes@gmail.com>
2023-02-03 15:21:48 +0300

    refactor: little

--- constants/addresses.ts
+++ constants/addresses.ts
@@ -8,8 +8,8 @@ export enum CONTRACT_LIST {
 
 export const CONTRACTS: Record<CONTRACT_LIST, Partial<DeployNetworks>> = {
   PETO: {
-    polygon: "0x1AC18c75fFBCcc172F7417730E1a706c0c00524f",
     opera: "0x8e933cd23c6cB61Bd37033555223De1Aeec54bC2",
     bsc: "0x5321dD3EF9C9B13Ed7280737bf72D3279F49b2EF",
+    polygon: "0x1AC18c75fFBCcc172F7417730E1a706c0c00524f",
   },
 };
